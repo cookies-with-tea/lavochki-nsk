d67101338247827c7f7bf6b655f96f37954c97d8
s1veme <kpdpav@gmail.com>
2022-09-27 20:18:57 +0300

    update(app/components/TheHeader): add telegram-link

--- app/components/TheHeader.tsx
+++ app/components/TheHeader.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components'
 import Image from 'next/image'
 import AvatarImage from '@/assets/profile-img.jpg'
@@ -46,23 +46,41 @@ const StyledLink = styled.a`
 `
 
 const TheHeader = (): any => {
+    useEffect(() => {
+      const script = document.createElement("script")
+      const script2 = document.createElement("script")
+    
+      script.src = "https://telegram.org/js/telegram-widget.js?19";
+      script.setAttribute("data-telegram-login", "bot")
+      script.setAttribute("data-size", "large")
+      script.setAttribute("data-onauth", "onTelegramAuth(user)")
+      script.setAttribute("data-request-access", "write")
+      script.async = true;
+      
+      const code = "function onTelegramAuth(user) {alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');}"
+      script2.innerHTML = code
+      
+      document.body.appendChild(script);
+      document.body.appendChild(script2);
+    
+      return () => {
+        document.body.removeChild(script);
+        document.body.removeChild(script2);
+      }
+    }, []);
     return (
         <StyledHeader>
             <div className="logo">logo</div>
             <div className="auth">
-                <StyledLink
-                    href="https://t.me/s1veme_timetable_bot?start=1"
-                    target="_blank"
-                >
-                    Привязать аккаунт
-                </StyledLink>
                 <div className="auth__title">Хренсберг</div>
                 <div className="auth__avatar">
                     <Image src={AvatarImage} alt="Image" />
                 </div>
             </div>
         </StyledHeader>
+        
     );
 };
 
 export default TheHeader;
+
