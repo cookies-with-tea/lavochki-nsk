8565ee9e40124eeff58d91fe690a1e9ca1669e38
s1veme <kpdpav@gmail.com>
2022-10-01 17:34:35 +0300

    update(internal/config): add SigningKey and Telegram

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,8 +6,9 @@ import (
 )
 
 type Config struct {
-	IsDebug       bool `env:"IS_DEBUG" env-default:"false"`
-	IsDevelopment bool `env:"IS_DEV" env-default:"false"`
+	IsDebug       bool   `env:"IS_DEBUG" env-default:"false"`
+	IsDevelopment bool   `env:"IS_DEV" env-default:"false"`
+	SigningKey    string `env:"SIGNING_KEY"`
 	Listen        struct {
 		BindIP string `env:"BIND_IP" env-default:"0.0.0.0"`
 		Port   string `env:"PORT" env-default:"8000"`
@@ -35,6 +36,9 @@ type Config struct {
 	}
 	Images struct {
 		PublicEndpoint string `env:"IMAGES_PUBLIC_ENDPOINT" env-default:"http://localhost:9000"`
+	}
+	Telegram struct {
+		Token string `env:"TELEGRAM_TOKEN"`
 	}
 }
 
