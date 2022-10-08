b375eb6d4318847038d79b73314e1620c4ff23b4
s1veme <kpdpav@gmail.com>
2022-10-08 19:50:16 +0300

    update(migrations): add role field in users

--- migrations/000001_inital_users_table.up.sql
+++ migrations/000001_inital_users_table.up.sql
@@ -3,7 +3,8 @@ BEGIN;
 CREATE TABLE users (
     ID TEXT NOT NULL PRIMARY KEY,
     username TEXT NOT NULL,
-    telegram_id INT NOT NULL
+    telegram_id INT NOT NULL,
+    role TEXT NOT NULL
 );
 
 COMMIT;
