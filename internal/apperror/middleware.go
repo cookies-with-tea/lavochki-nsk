03c869e2f32b8a294db0fc6ce746edabe1c1ffc9
s1veme <kpdpav@gmail.com>
2022-10-14 10:08:43 +0300

    add(internal/handlers/apperror): add apperror

---
+++ internal/apperror/middleware.go
@@ -0,0 +1,43 @@
+package apperror
+
+import (
+	"errors"
+	"net/http"
+)
+
+type appHandler func(w http.ResponseWriter, r *http.Request) error
+
+func Middleware(h appHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		var appErr *AppError
+		err := h(w, r)
+		if err != nil {
+			if errors.As(err, &appErr) {
+				if errors.Is(err, ErrNotFound) {
+					w.WriteHeader(http.StatusNotFound)
+					w.Write(ErrNotFound.Marshal())
+					return
+				}
+				if errors.Is(err, ErrIncorrectDataAuth) {
+					w.WriteHeader(http.StatusNotFound)
+					w.Write(ErrIncorrectDataAuth.Marshal())
+					return
+				}
+				if errors.Is(err, ErrIncorrectDataToken) {
+					w.WriteHeader(http.StatusNotFound)
+					w.Write(ErrIncorrectDataToken.Marshal())
+					return
+				}
+				err = err.(*AppError)
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write(appErr.Marshal())
+				return
+			}
+
+			w.WriteHeader(http.StatusTeapot)
+			w.Write(systemError(err).Marshal())
+		}
+	}
+}
