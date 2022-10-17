5788179bf7fdf47c1dc545f7d160048ae9e871f9
s1veme <kpdpav@gmail.com>
2022-10-17 08:49:54 +0300

    add(internal/dto): add CreateBenchViaTelegram

--- internal/dto/benches.go
+++ internal/dto/benches.go
@@ -6,6 +6,13 @@ type CreateBench struct {
 	Image []byte  `json:"image"`
 }
 
+type CreateBenchViaTelegram struct {
+	Lat            float64 `json:"lat"`
+	Lng            float64 `json:"lng"`
+	Image          []byte  `json:"image"`
+	UserTelegramID int     `json:"user_telegram_id"`
+}
+
 type DecisionBench struct {
 	ID       string `json:"id"`
 	Decision bool   `json:"decision"`
