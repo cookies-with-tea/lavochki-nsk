ea443ea0338f6b858691c83e3e0084271520b446
Andrey <kpdpav@gmail.com>
2022-10-20 14:57:43 +0300

    update(internal/service/notifications): update mock, add return error

--- internal/service/notifications/mock.go
+++ internal/service/notifications/mock.go
@@ -17,7 +17,8 @@ func NewServiceMock(log *zap.Logger, telegramNotificationURL string) *ServiceMoc
 	}
 }
 
-func (s *ServiceMock) SendNotificationInTelegram(ctx context.Context, typeNotification string, userID int, benchID string) {
+func (s *ServiceMock) SendNotificationInTelegram(ctx context.Context, typeNotification string, userID int, benchID string) error {
 	s.log.Info("Send message to telegram", zap.String("notification", typeNotification),
 		zap.Int("user", userID), zap.String("bench", benchID))
+	return nil
 }
