772d4c9646d422f250ce413ef5b9a7512a22c378
Andrey <kpdpav@gmail.com>
2022-10-24 07:33:27 +0300

    update(migrations): update field image in benches, image to []TEXT and renamed to images

--- migrations/000002_inital_benches_table.up.sql
+++ migrations/000002_inital_benches_table.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE benches (
     id TEXT NOT NULL PRIMARY KEY,
     lat decimal,
     lng decimal,
-    image TEXT,
+    images TEXT[],
     owner_id TEXT,
     is_active BOOLEAN DEFAULT false,
     CONSTRAINT fk_owner
