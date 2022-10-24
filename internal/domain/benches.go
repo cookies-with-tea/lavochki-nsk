9dbb61f89b505d055864c574ec443849fe23dc2c
Andrey <kpdpav@gmail.com>
2022-10-24 07:34:37 +0300

    update(internal): update field Image, renamed Image to Images

--- internal/domain/benches.go
+++ internal/domain/benches.go
@@ -1,10 +1,10 @@
 package domain
 
 type Bench struct {
-	ID       string  `json:"id,pk"`
-	Lat      float64 `json:"lat"`
-	Lng      float64 `json:"lng"`
-	Image    string  `json:"image"`
-	IsActive bool    `json:"is_active"`
-	Owner    *User   `json:"-"`
+	ID       string   `json:"id,pk"`
+	Lat      float64  `json:"lat"`
+	Lng      float64  `json:"lng"`
+	Images   []string `json:"images"`
+	IsActive bool     `json:"is_active"`
+	Owner    *User    `json:"-"`
 }
--- internal/dto/benches.go
+++ internal/dto/benches.go
@@ -9,10 +9,10 @@ type CreateBench struct {
 }
 
 type CreateBenchViaTelegram struct {
-	Lat            float64 `json:"lat"`
-	Lng            float64 `json:"lng"`
-	Image          []byte  `json:"image"`
-	UserTelegramID int     `json:"user_telegram_id"`
+	Lat            float64  `json:"lat"`
+	Lng            float64  `json:"lng"`
+	Images         [][]byte `json:"images"`
+	UserTelegramID int      `json:"user_telegram_id"`
 }
 
 type DecisionBench struct {
@@ -31,7 +31,7 @@ func (bench *CreateBenchViaTelegram) Validate() error {
 	return validation.ValidateStruct(bench,
 		validation.Field(&bench.Lat, validation.Required),
 		validation.Field(&bench.Lng, validation.Required),
-		validation.Field(&bench.Image, validation.Required),
+		validation.Field(&bench.Images, validation.Required),
 		validation.Field(&bench.UserTelegramID, validation.Required))
 }
 
