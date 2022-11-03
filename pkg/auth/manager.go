b2d17a43a4efaa6c6353e036c9b27692ea4cee5d
Andrey <kpdpav@gmail.com>
2022-11-03 16:13:31 +0300

    review(pkg/auth): delete Println

--- pkg/auth/manager.go
+++ pkg/auth/manager.go
@@ -84,7 +84,6 @@ func (m *Manager) JWTMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authHeader := strings.Split(r.Header.Get("Authorization"), "Bearer ")
 		if len(authHeader) != 2 {
-			fmt.Println("Malformed token")
 			w.WriteHeader(http.StatusUnauthorized)
 			_, err := w.Write([]byte("Malformed Token"))
 			if err != nil {
