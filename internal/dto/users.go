94d6c764845f4b7097d2555d4797f71e94baa333
Andrey <kpdpav@gmail.com>
2022-11-03 15:55:07 +0300

    add(internal/dto): add AuthorizationBot

--- internal/dto/users.go
+++ internal/dto/users.go
@@ -12,6 +12,11 @@ type CreateUser struct {
 	Hash      string `json:"hash"`
 }
 
+type AuthorizationBot struct {
+	Login    string
+	Password string
+}
+
 func (u *CreateUser) Validate() error {
 	return validation.ValidateStruct(u,
 		validation.Field(&u.ID, validation.Required),
@@ -21,4 +26,11 @@ func (u *CreateUser) Validate() error {
 		validation.Field(&u.PhotoUrl, validation.Required),
 		validation.Field(&u.AuthDate, validation.Required),
 		validation.Field(&u.Hash, validation.Required))
+}
+
+func (u *AuthorizationBot) Validate() error {
+	return validation.ValidateStruct(u,
+		validation.Field(&u.Login, validation.Required),
+		validation.Field(&u.Password, validation.Required),
+	)
 }
