634d79f44ee79a5427cfb957c4ae61d097fef304
Andrey <kpdpav@gmail.com>
2022-11-07 15:56:27 +0300

    add(internal/handlers/tags): add method createTag

--- internal/handlers/tags/tags.go
+++ internal/handlers/tags/tags.go
@@ -2,7 +2,9 @@ package tags
 
 import (
 	"benches/internal/apperror"
+	"benches/internal/dto"
 	"benches/internal/service/tags"
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"net/http"
 )
@@ -19,7 +21,8 @@ func NewTagsHandler(service tags.Service) *Handler {
 }
 
 func (h *Handler) Register(router *mux.Router) {
-	router.HandleFunc("/", apperror.Middleware(h.listTags)).Methods("GET")
+	router.HandleFunc("", apperror.Middleware(h.listTags)).Methods("GET")
+	router.HandleFunc("", apperror.Middleware(h.createTag)).Methods("POST")
 }
 
 // @Summary List tags
@@ -27,12 +30,40 @@ func (h *Handler) Register(router *mux.Router) {
 // @Tags Tags
 // @Success 200 {object} []domain.Tag
 // @Failure 400 {object} apperror.AppError
-// @Router /api/v1/tags/ [get]
+// @Router /api/v1/tags [get]
 func (h *Handler) listTags(w http.ResponseWriter, r *http.Request) error {
 	listTags, err := h.service.GetAllTags(r.Context())
 	if err != nil {
 		return err
 	}
 	h.ResponseJson(w, listTags, http.StatusOK)
+	return nil
+}
+
+// @Summary Create tag
+// @Description Create tag
+// @Tags Tags
+// @Param CreateTag body dto.CreateTag true "tag data"
+// @Failure 400 {object} apperror.AppError
+// @Failure 403 {object} apperror.AppError
+// @Router /api/v1/tags [post]
+func (h *Handler) createTag(w http.ResponseWriter, r *http.Request) error {
+	var tag dto.CreateTag
+	if err := json.NewDecoder(r.Body).Decode(&tag); err != nil {
+		return apperror.ErrDecodeData
+	}
+
+	// Валидация
+	if err := tag.Validate(); err != nil {
+		details, _ := json.Marshal(err)
+		return apperror.NewAppError(err, "validation error", details)
+	}
+
+	err := h.service.CreateTag(r.Context(), tag)
+	if err != nil {
+		return err
+	}
+
+	h.ResponseJson(w, nil, http.StatusCreated)
 	return nil
 }
