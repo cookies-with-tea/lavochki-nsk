59b5b977da2e221d5cd421a07804ed3d07c7317b
Andrey <kpdpav@gmail.com>
2022-11-07 15:47:53 +0300

    add(internal/dto): add method Validate for CreateTag

--- internal/dto/tags.go
+++ internal/dto/tags.go
@@ -1,5 +1,12 @@
 package dto
 
+import validation "github.com/go-ozzo/ozzo-validation"
+
 type CreateTag struct {
 	Title string `json:"title"`
+}
+
+func (tag *CreateTag) Validate() error {
+	return validation.ValidateStruct(tag,
+		validation.Field(&tag.Title, validation.Required))
 }
