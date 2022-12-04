ac8352e95fbff3c8987c7f770b06fb6e448a6ae1
miirsery <miirsery.main@gmail.com>
2022-12-04 19:07:34 +0700

    F-12: Add: styles, data in bench card

---
+++ app/components/pages/BenchesPage/BenchCard/BenchCard.tsx
@@ -0,0 +1,49 @@
+import React, {FC} from 'react';
+import {IProps} from "@/app/components/pages/BenchesPage/BenchCard/BenchesCard.interface";
+import Image from "next/image";
+import {
+    StyledBenchCard,
+    StyledBenchTitle,
+    StyledBenchWrapper,
+    StyledImageWrapper,
+    StyledOpenButton,
+    StyledRatingCount,
+    StyledTag,
+    StyledTags
+} from "@/app/components/pages/BenchesPage/BenchCard/BenchesCard.styles";
+import CommonIcon from "@/app/components/Common/CommonIcon/CommonIcon";
+
+const BenchCard: FC<IProps> = ({ bench }): JSX.Element => {
+    return (
+        <StyledBenchCard>
+            <StyledImageWrapper>
+                <Image src={bench.images[0]} alt="image" width="100%" height="100%" layout="responsive" objectFit="contain" />
+            </StyledImageWrapper>
+            <StyledBenchWrapper>
+                <StyledBenchTitle>Лавочка на Каменской</StyledBenchTitle>
+                <StyledTags>
+                    {
+                        bench.tags.map((item: any) => (
+                            <StyledTag key={item.id}>
+                                #{item.title}
+                            </StyledTag>
+                        ) )
+                    }
+                </StyledTags>
+                <div className={"d-flex ai-center jc-between"}>
+                    <StyledOpenButton>
+                        Открыть
+                    </StyledOpenButton>
+                    <div className={"d-flex ai-center"}>
+                        <CommonIcon name="star" width={16} height={16} />
+                        <StyledRatingCount>
+                            4.2
+                        </StyledRatingCount>
+                    </div>
+                </div>
+            </StyledBenchWrapper>
+        </StyledBenchCard>
+    );
+};
+
+export default BenchCard;
---
+++ app/components/pages/BenchesPage/BenchCard/BenchesCard.styles.ts
@@ -0,0 +1,69 @@
+import styled from "@emotion/styled";
+import {Button} from "@mui/material";
+
+export const StyledBenchCard = styled.div`
+  width: 300px;
+  height: 400px;
+  border-radius: 20px;
+  background-color: #F0E7DF;
+  
+  span {
+    &:first-of-type {
+      height: 200px !important;
+      border-top-left-radius: 20px;
+      border-top-right-radius: 20px;
+    }
+  }
+`
+
+export const StyledBenchWrapper = styled.div`
+  padding: 0 38px 33px 32px;
+`
+
+export const StyledImageWrapper = styled.div`
+  width: 100%;
+  height: 200px;
+  margin-bottom: 32px;
+`
+
+export const StyledBenchTitle = styled.div`
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 22px;
+  color: var(--color--primary-dark);
+  margin-bottom: 34px;
+`
+
+export const StyledTags = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    width: 100%;
+    margin-bottom: 18px;
+`
+
+export const StyledTag = styled(Button)`
+  font-size: 14px;
+  line-height: 16px;
+  color: var(--color--primary-dark);
+  padding: 0 !important;
+  border: none !important;
+  text-transform: capitalize;
+  margin-right: 10px;
+  text-align: left !important;
+  display: block;
+`
+
+export const StyledOpenButton = styled(Button)`
+  border: 1px solid var(--color--primary-dark) !important;
+  border-radius: 100px;
+  background-color: transparent !important;
+  padding: 7px 20px !important;
+`
+
+export const StyledRatingCount = styled.div`
+  font-size: 16px;
+  line-height: 18px;
+  color: var(--color--primary-dark);
+  margin-left: 4px;
+`
