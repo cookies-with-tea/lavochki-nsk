5ef3dfd05d86a4b686da9e9f8a6a01fe0ad036a6
Andrey <kpdpav@gmail.com>
2022-12-07 19:02:34 +0300

    update(internal/domain): add field Comments in Bench

--- internal/domain/benches.go
+++ internal/domain/benches.go
@@ -1,11 +1,12 @@
 package domain
 
 type Bench struct {
-	ID       string   `json:"id,pk"`
-	Lat      float64  `json:"lat"`
-	Lng      float64  `json:"lng"`
-	Images   []string `json:"images"`
-	IsActive bool     `json:"is_active"`
-	Tags     []Tag    `json:"tags"`
-	Owner    *User    `json:"-"`
+	ID       string    `json:"id,pk"`
+	Lat      float64   `json:"lat"`
+	Lng      float64   `json:"lng"`
+	Images   []string  `json:"images"`
+	IsActive bool      `json:"is_active"`
+	Tags     []Tag     `json:"tags"`
+	Comments []Comment `json:"comments"`
+	Owner    *User     `json:"-"`
 }
