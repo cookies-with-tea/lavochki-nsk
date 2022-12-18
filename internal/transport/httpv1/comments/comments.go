62fe8d76bcc63a72082df2073d8feafa965eb689
Andrey <kpdpav@gmail.com>
2022-12-18 21:38:42 +0300

    add(internal/transport/httpv1/comments): add method createComment

--- internal/transport/httpv1/comments/comments.go
+++ internal/transport/httpv1/comments/comments.go
@@ -2,8 +2,11 @@ package comments
 
 import (
 	"benches/internal/apperror"
+	"benches/internal/dto"
 	commentsService "benches/internal/service/comments"
+	usersService "benches/internal/service/users"
 	"database/sql"
+	"encoding/json"
 	"errors"
 	"github.com/gorilla/mux"
 	"net/http"
@@ -11,12 +14,14 @@ import (
 
 type Handler struct {
 	baseHandler
-	comments commentsService.Service
+	comments     commentsService.Service
+	usersService usersService.Service
 }
 
-func NewCommentsHandler(comments commentsService.Service) *Handler {
+func NewCommentsHandler(comments commentsService.Service, usersService usersService.Service) *Handler {
 	return &Handler{
-		comments: comments,
+		comments:     comments,
+		usersService: usersService,
 	}
 }
 
@@ -36,5 +41,38 @@ func (handler *Handler) listCommentsByBench(writer http.ResponseWriter, request 
 	}
 
 	handler.ResponseJson(writer, comments, http.StatusOK)
+	return nil
+}
+
+func (handler *Handler) createComment(writer http.ResponseWriter, request *http.Request) error {
+	var comment dto.CreateComment
+
+	// Получение пользователя, который хочет создать комментарий
+	userID := request.Context().Value("userID")
+	user, errGetUser := handler.usersService.GetUserByID(request.Context(), userID.(string))
+	if errGetUser != nil {
+		return errGetUser
+	}
+
+	if err := json.NewDecoder(request.Body).Decode(&comment); err != nil {
+		return apperror.ErrDecodeData
+	}
+
+	// Валидация
+	if errValidate := comment.Validate(); errValidate != nil {
+		details, _ := json.Marshal(errValidate)
+		return apperror.NewAppError(errValidate, "validation error", details)
+	}
+
+	// Создание нового комментария
+	commentDomain := comment.ToDomain()
+	commentDomain.AuthorID = user.ID
+
+	errCreateComment := handler.comments.CreateComment(request.Context(), commentDomain)
+	if errCreateComment != nil {
+		return errCreateComment
+	}
+
+	writer.WriteHeader(http.StatusCreated)
 	return nil
 }
