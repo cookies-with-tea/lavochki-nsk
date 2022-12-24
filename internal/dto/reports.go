d882db716b8910ce2c8faa3a3500cb395b59b4ff
Andrey <kpdpav@gmail.com>
2022-12-24 20:42:14 +0300

    add(internal/dto): add CreateReportComment

---
+++ internal/dto/reports.go
@@ -0,0 +1,27 @@
+package dto
+
+import (
+	"benches/internal/domain"
+	validation "github.com/go-ozzo/ozzo-validation"
+)
+
+type CreateReportComment struct {
+	ID        string `json:"id"`
+	Cause     string `json:"cause"`
+	CommentID string `json:"comment_id"`
+}
+
+func (report *CreateReportComment) Validate() error {
+	return validation.ValidateStruct(report,
+		validation.Field(&report.ID, validation.Required),
+		validation.Field(&report.CommentID, validation.Required),
+		validation.Field(&report.Cause, validation.Required))
+}
+
+func (report *CreateReportComment) ToDomain() domain.CommentReport {
+	return domain.CommentReport{
+		ID:        report.ID,
+		Cause:     report.Cause,
+		CommentID: report.CommentID,
+	}
+}
