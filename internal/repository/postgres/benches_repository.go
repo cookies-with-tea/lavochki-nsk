243db38a5c624d8b5c50014d48c2af5afee26d3c
Andrey <kpdpav@gmail.com>
2022-12-31 09:04:28 +0300

    add(internal/repository/postgres): add sortOptions in GetBenches

--- internal/repository/postgres/benches_repository.go
+++ internal/repository/postgres/benches_repository.go
@@ -2,13 +2,14 @@ package postgres
 
 import (
 	"benches/internal/domain"
+	"benches/internal/repository/model"
 	"context"
 	"github.com/oklog/ulid/v2"
 	"github.com/uptrace/bun"
 )
 
 type BenchesRepository interface {
-	GetBenches(ctx context.Context, isActive bool) ([]domain.Bench, error)
+	GetBenches(ctx context.Context, isActive bool, sortOptions model.SortOptions) ([]domain.Bench, error)
 	CreateBench(ctx context.Context, bench domain.Bench) error
 	UpdateActiveBench(ctx context.Context, id string, decision bool) error
 	DeleteBench(ctx context.Context, id string) error
@@ -25,9 +26,10 @@ func NewBenchesRepository(db *bun.DB) BenchesRepository {
 	}
 }
 
-func (b *benchesRepository) GetBenches(ctx context.Context, isActive bool) ([]domain.Bench, error) {
+func (b *benchesRepository) GetBenches(ctx context.Context, isActive bool, sortOptions model.SortOptions) ([]domain.Bench, error) {
 	benchesModel := make([]benchModel, 0)
 	err := b.db.NewSelect().Model(&benchesModel).Where("is_active = ?", isActive).
+		Order(sortOptions.GetOrderBy()).
 		Relation("Owner").
 		Relation("Tags").
 		Scan(ctx)
