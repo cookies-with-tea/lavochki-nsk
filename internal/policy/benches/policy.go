0b799232c96e658926ad1d354497e58e491d1422
Andrey <kpdpav@gmail.com>
2023-01-05 13:54:43 +0300

    add(internal): add policy benches

---
+++ internal/policy/benches/policy.go
@@ -0,0 +1,92 @@
+package benches
+
+import (
+	"benches/internal/apperror"
+	"benches/internal/domain"
+	"benches/internal/notifications"
+	benchesService "benches/internal/service/benches"
+	notificationsService "benches/internal/service/notifications"
+	usersService "benches/internal/service/users"
+	"benches/pkg/api/sort"
+	"context"
+)
+
+type Policy struct {
+	benchesService       benchesService.Service
+	usersService         usersService.Service
+	notificationsService notificationsService.Service
+}
+
+func NewBenchesPolicy(benchesService benchesService.Service, usersService usersService.Service, notificationsService notificationsService.Service) *Policy {
+	return &Policy{benchesService: benchesService, usersService: usersService, notificationsService: notificationsService}
+}
+
+func (policy *Policy) CreateBenchViaTelegram(ctx context.Context, userTelegramID int, byteImages [][]byte, bench domain.Bench) error {
+	// Получаем пользователя по Telegram ID
+	user, err := policy.usersService.ByTelegramID(ctx, userTelegramID)
+	if err != nil {
+		return apperror.ErrNotEnoughRights
+	}
+
+	// Сохраняем все фотографии, которые нам пришли в виде байт, в Minio
+	images, err := policy.benchesService.SaveImages(ctx, byteImages)
+	if err != nil {
+		return err
+	}
+
+	// Дополняем поля Bench
+	bench.Images = images
+	bench.Owner = user.ID
+
+	// Создаём лавочку
+	errCreateBench := policy.benchesService.CreateBench(ctx, bench)
+	if errCreateBench != nil {
+		return errCreateBench
+	}
+
+	return nil
+}
+
+func (policy *Policy) GetListBenches(ctx context.Context, isActive bool, sortOptions sort.Options) ([]*domain.Bench, error) {
+	return policy.benchesService.GetListBenches(ctx, isActive, sortOptions)
+}
+
+func (policy *Policy) DecisionBench(ctx context.Context, benchID string, decision bool, message string) error {
+	// Получаем лавочку по ID, потому что нам нужен TelegramID создателя лавочки
+	bench, errGetBench := policy.benchesService.GetBenchByID(ctx, benchID)
+	if errGetBench != nil {
+		return errGetBench
+	}
+
+	// Получаем пользователя по ID
+	user, errGetUser := policy.usersService.GetUserByID(ctx, bench.Owner)
+	if errGetUser != nil {
+		return errGetUser
+	}
+
+	// Обновляем bench. Если decision = false, то лавочка удалится. Иначе будет обновлено поле is_active = true
+	errDecisionBench := policy.benchesService.DecisionBench(ctx, benchID, decision)
+	if errDecisionBench != nil {
+		return errDecisionBench
+	}
+
+	// Выбираем тип уведомления
+	var notification *notifications.TelegramNotification
+	if decision {
+		notification = notifications.BenchSuccessfullyAccepted.ToNotification(user.TelegramID, bench.ID)
+	} else {
+		notification = notifications.BenchDenied.ToNotification(user.TelegramID, bench.ID)
+	}
+
+	// Отправляем уведомления в telegram
+	errSendNotification := policy.notificationsService.SendNotificationInTelegram(ctx, notification)
+	if errSendNotification != nil {
+		return errSendNotification
+	}
+
+	return nil
+}
+
+func (policy *Policy) GetBenchByID(ctx context.Context, id string) (*domain.Bench, error) {
+	return policy.benchesService.GetBenchByID(ctx, id)
+}
