4b00fffce81eaf6757cbcc3f0b52b2342623f4dc
Andrey <kpdpav@gmail.com>
2023-01-05 13:43:04 +0300

    add(internal/repository/postgres): add interface Client PostgreSQL

---
+++ internal/repository/postgres/interfaces.go
@@ -0,0 +1,14 @@
+package postgres
+
+import (
+	"context"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+type Client interface {
+	Begin(context.Context) (pgx.Tx, error)
+	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
+	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
+}
