dd87396277781bffb7712d3942d298c556d8ba35
Andrey <kpdpav@gmail.com>
2023-01-05 13:53:39 +0300

    update(internal/domain): update field NestedComments, now it's a list of pointers

--- internal/domain/comments.go
+++ internal/domain/comments.go
@@ -1,10 +1,10 @@
 package domain
 
 type Comment struct {
-	ID             string    `json:"id,pk"`
-	BenchID        string    `json:"bench_id"`
-	ParentID       string    `json:"parent_id"`
-	AuthorID       string    `json:"author_id"`
-	Content        string    `json:"content"`
-	NestedComments []Comment `json:"nested_comments"`
+	ID             string     `json:"id,pk"`
+	BenchID        string     `json:"bench_id"`
+	ParentID       string     `json:"parent_id"`
+	AuthorID       string     `json:"author_id"`
+	Content        string     `json:"content"`
+	NestedComments []*Comment `json:"nested_comments"`
 }
