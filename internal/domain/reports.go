01abdcafc67edaefc889f764a139dd6fdc408dc8
Andrey <kpdpav@gmail.com>
2023-01-14 16:42:56 +0300

    update(internal/domain): update json field in CommentReport

--- internal/domain/reports.go
+++ internal/domain/reports.go
@@ -3,7 +3,7 @@ package domain
 type CommentReport struct {
 	ID        string `json:"ID"`
 	Cause     string `json:"cause"`
-	CommentID string `json:"commentID"`
-	UserID    string `json:"userID"`
+	CommentID string `json:"comment_id"`
+	UserID    string `json:"user_id"`
 	IsActive  bool   `json:"is_active"`
 }
