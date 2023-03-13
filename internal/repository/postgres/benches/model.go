553c42d4977c164fbe79654e418e17fefb816978
Andrey <kpdpav@gmail.com>
2023-03-13 12:19:50 +0300

    update(internal/repository/postgres/benches): add field street in benchModel

--- internal/repository/postgres/benches/model.go
+++ internal/repository/postgres/benches/model.go
@@ -2,16 +2,18 @@ package benches
 
 import (
 	"benches/internal/domain"
+	"database/sql"
 	"github.com/mitchellh/mapstructure"
 )
 
 type benchModel struct {
-	ID       string   `mapstructure:"id,omitempty"`
-	Lat      float64  `mapstructure:"lat,omitempty"`
-	Lng      float64  `mapstructure:"lng,omitempty"`
-	Images   []string `mapstructure:"images,omitempty"`
-	IsActive bool     `mapstructure:"is_active,omitempty"`
-	OwnerID  string   `mapstructure:"owner_id,omitempty"`
+	ID       string         `mapstructure:"id,omitempty"`
+	Lat      float64        `mapstructure:"lat,omitempty"`
+	Lng      float64        `mapstructure:"lng,omitempty"`
+	Street   sql.NullString `mapstructure:"street,omitempty"`
+	Images   []string       `mapstructure:"images,omitempty"`
+	IsActive bool           `mapstructure:"is_active,omitempty"`
+	OwnerID  string         `mapstructure:"owner_id,omitempty"`
 }
 
 func (b *benchModel) FromDomain(bench domain.Bench) {
@@ -21,10 +23,11 @@ func (b *benchModel) FromDomain(bench domain.Bench) {
 	b.Images = bench.Images
 	b.IsActive = bench.IsActive
 	b.OwnerID = bench.Owner
+	b.Street = sql.NullString{String: bench.Street, Valid: true}
 }
 
 func benchModelToDomain(model benchModel) domain.Bench {
-	return domain.Bench{
+	bench := domain.Bench{
 		ID:       model.ID,
 		Lat:      model.Lat,
 		Lng:      model.Lng,
@@ -32,12 +35,24 @@ func benchModelToDomain(model benchModel) domain.Bench {
 		IsActive: model.IsActive,
 		Owner:    model.OwnerID,
 	}
+
+	streetValue, err := model.Street.Value()
+	if err != nil {
+		return domain.Bench{}
+	}
+
+	if value, ok := streetValue.(string); ok {
+		bench.Street = value
+	}
+
+	return bench
 }
 
-func benchModelsToDomain(models []benchModel) []domain.Bench {
-	benches := make([]domain.Bench, 0, len(models))
+func benchModelsToDomain(models []benchModel) []*domain.Bench {
+	benches := make([]*domain.Bench, 0, len(models))
 	for _, model := range models {
-		benches = append(benches, benchModelToDomain(model))
+		bench := benchModelToDomain(model)
+		benches = append(benches, &bench)
 	}
 	return benches
 }
