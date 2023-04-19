5df93145f7c9e02707f3c89ef6042a7e40daee50
miirsery <miirsery.main@gmail.com>
2023-04-19 19:55:07 +0700

    test husky

--- src/components/layouts/MainLayout/MainLayoutSidebar/MainLayoutSidebar.style.ts
+++ src/components/layouts/MainLayout/MainLayoutSidebar/MainLayoutSidebar.style.ts
@@ -1,5 +1,5 @@
-import { Box, ThemeOptions } from '@mui/material'
 import styled from '@emotion/styled'
+import { Box, ThemeOptions } from '@mui/material'
 export interface ICustomTheme extends ThemeOptions {
   palette: {
     primary: {
