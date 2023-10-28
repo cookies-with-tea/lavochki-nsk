de312698ebbbd23cfd6727aabf96c93f1dbc13ed
miirsery <miirsery.main@gmail.com>
2023-10-28 23:39:13 +0700

    feat(): add styles for 500 error page

--- src/pages/error-500/ui/Error500.tsx
+++ src/pages/error-500/ui/Error500.tsx
@@ -1,7 +1,69 @@
+import { useEffect, useRef, useState } from 'react'
+
+import styles from 'pages/error-500/ui/styles.module.scss'
+
+import { SIcon } from 'shared/ui'
+
 export const Error500 = () => {
+  const gearRefs = useRef<Array<HTMLDivElement>>([])
+  const startTime = useRef(performance.now())
+  const [rAF, setRAF] = useState<number>()
+
+  // Функция для анимации поворота
+  const animate = () => {
+    const elapsedTime = (performance.now() - startTime.current) / 1000 // Преобразование времени в секунды
+
+    gearRefs.current.forEach((gearRef, index) => {
+      if (gearRef) {
+        let rotationSpeed = 50 // Скорость вращения по умолчанию в градусах в секунду
+
+        if (index % 2 !== 0) {
+          // Если индекс нечетный (второй компонент), уменьшаем скорость вращения
+          rotationSpeed = 50 // Новая скорость вращения для второго компонента
+        }
+
+        const rotation = ((index % 2 === 0 ? 1 : -1) * elapsedTime * rotationSpeed) % 360
+
+        gearRef.style.transform = `rotate(${rotation + 21}deg)`
+      }
+    })
+    const localRAF = requestAnimationFrame(animate)
+
+    setRAF(localRAF)
+  }
+
+  useEffect(() => {
+    // Инициализация refs для каждого компонента SIcon
+    gearRefs.current = gearRefs.current.slice(0, 2)
+
+    // Запуск анимации
+    animate()
+
+    // Очистка refs при размонтировании компонента
+    return () => {
+      gearRefs.current = []
+
+      if (rAF) {
+        cancelAnimationFrame(rAF)
+      }
+    }
+  }, [])
+
   return (
-    <div>
-      Error 500
+    <div className={styles['error-500']}>
+      <div>
+        <h1>Произошла ошибка</h1>
+        <h2 className={'mt-28'}>Ведутся технические работы</h2>
+
+        <div className={styles['error-500__gears']}>
+          <div className={styles['error-500__gear']} ref={(el) => { if (el) gearRefs.current[0] = el }}>
+            <SIcon name={'gear'} />
+          </div>
+          <div className={styles['error-500__gear']} ref={(el) => { if (el) gearRefs.current[1] = el }}>
+            <SIcon name={'gear'} />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
