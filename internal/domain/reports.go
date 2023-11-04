433f24f1cd69586ac6c2f675a83f41cebbed5485
Andrey <kpdpav@gmail.com>
2023-11-04 18:56:41 +0300

    fix(internal/domain): fix json tag for ID

--- internal/domain/reports.go
+++ internal/domain/reports.go
@@ -1,7 +1,7 @@
 package domain
 
 type CommentReport struct {
-	ID        string `json:"ID"`
+	ID        string `json:"id"`
 	Cause     string `json:"cause"`
 	CommentID string `json:"comment_id"`
 	UserID    string `json:"user_id"`
